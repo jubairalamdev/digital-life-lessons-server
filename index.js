baf0f3796d75fc5cd9c4ba7fe89851773fbe1e2f
Jubair Alam <jubairalam.dev@gmail.com>
2026-06-23 19:37:27 +0600

    Starting the Backend server, setup done for initial run and pre-requisities.

---
+++ index.js
@@ -0,0 +1,19 @@
+const dns = require("node:dns");
+dns.setServers(['8.8.8.8', '8.8.4.4'])
+
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('The Root of Server is here')
+})
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+})
