4a8ce04bedde73b8036831f90f45a597b7e6c9d5
Jubair Alam <jubairalam.dev@gmail.com>
2026-06-23 20:13:31 +0600

    Dotenv set

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const dns = require("node:dns");
 dns.setServers(['8.8.8.8', '8.8.4.4'])
 
+require('dotenv').config();
+
 const express = require('express')
 const cors = require('cors')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
